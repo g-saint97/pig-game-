62fa5b72d418c5af066a1289e2ce52cd55637cd4
George Saint Hilaire <91929584+g-saint97@users.noreply.github.com>
2023-04-24 17:21:27 -0400

    Update script.js
    adding the javascript

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const modalBtn = document.querySelector('.show-modal');
+const modalRules = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay')
+const closeModal = document.querySelector('.close-modal');
+
 let scores, currentScore, activePlayer, playing;
 
 // Starting conditions
@@ -57,8 +62,9 @@ btnRoll.addEventListener('click', function () {
     if (dice !== 1) {
       // Add dice to current score
       currentScore += dice;
-      document.getElementById(`current--${activePlayer}`).textContent =
-        currentScore;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
     } else {
       // Switch to next player
       switchPlayer();
@@ -93,5 +99,15 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+modalBtn.addEventListener('click', function () {
+  modalRules.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+});
+
+closeModal.addEventListener('click', function(){
+  modalRules.classList.add('hidden');
+  overlay.classList.add('hidden');
+})
 
 btnNew.addEventListener('click', init);
